edff34e588898bea7704d487e0b62d9aff0f3c65
Tiranrumesh <Nishandananjaya23@gmail.com>
2025-09-25 18:45:48 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=2513086a5bd9546a' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '[![.github/workflows/main.yml](https://github.com/Tiranrumesh/rukshanmb/actions/workflows/main.yml/badge.svg)](https://github.com/Tiranrumesh/rukshanmb/actions/workflows/main.yml)' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
